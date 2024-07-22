5e8a4c17de778d30b4fa88dd45bad0e58467d77a
Oleksii Avramenko <oleksii.avramenko.42@gmail.com>
2024-07-22 17:55:23 +0800

    floating nvimtree with zenmode

--- lua/plugins/configs/nvimtree.lua
+++ lua/plugins/configs/nvimtree.lua
@@ -17,6 +17,94 @@ local function get_centered_window_config()
   }
 end
 
+local log_file = vim.fn.stdpath('data') .. '/zen_tree_debug.log'
+
+
+local function log_message(msg)
+    local file = io.open(log_file, "a")
+    if file then
+        file:write(os.date("%Y-%m-%d %H:%M:%S") .. " " .. msg .. "\n")
+        file:close()
+    end
+end
+
+local function zen_window_picker()
+    log_message("Entering zen_window_picker function")
+    local windows = vim.api.nvim_list_wins()
+    local zen_window = nil
+
+    log_message("Total windows: " .. #windows)
+
+    for _, win in ipairs(windows) do
+        local buf = vim.api.nvim_win_get_buf(win)
+        local win_config = vim.api.nvim_win_get_config(win)
+        local filetype = vim.bo[buf].filetype
+        local bufname = vim.fn.bufname(buf)
+
+        local log_entry = string.format(
+            "Window %d:\n" ..
+            "  Buffer: %d\n" ..
+            "  Filetype: %s\n" ..
+            "  Bufname: %s\n" ..
+            "  Is floating: %s\n" ..
+            "  Width: %d\n" ..
+            "  Height: %d\n" ..
+            "  Is current: %s\n",
+            win,
+            buf,
+            filetype,
+            bufname,
+            tostring(win_config.relative ~= ''),
+            win_config.width or vim.api.nvim_win_get_width(win),
+            win_config.height or vim.api.nvim_win_get_height(win),
+            tostring(vim.api.nvim_get_current_win() == win)
+        )
+
+        log_message(log_entry)
+
+        if win_config.relative ~= '' and 
+           filetype ~= 'NvimTree' and 
+           filetype ~= 'zenmode-bg' and
+           bufname ~= '' then
+            zen_window = win
+            log_message("Selecting as potential zen window: " .. win)
+        end
+    end
+
+    log_message("ZenMode active: " .. tostring(vim.g.zen_mode_active or false))
+    log_message("Final selected zen window: " .. tostring(zen_window))
+
+    return zen_window
+end
+
+local function open_in_zen_window(file_path)
+    local zen_window
+    for _, win in ipairs(vim.api.nvim_list_wins()) do
+        local buf = vim.api.nvim_win_get_buf(win)
+        local win_config = vim.api.nvim_win_get_config(win)
+        local filetype = vim.bo[buf].filetype
+        local bufname = vim.fn.bufname(buf)
+
+        if win_config.relative ~= '' and 
+           filetype ~= 'NvimTree' and 
+           filetype ~= 'zenmode-bg' and
+           bufname ~= '' then
+            zen_window = win
+            break
+        end
+    end
+
+    if zen_window then
+        -- Switch to the ZenMode window
+        vim.api.nvim_set_current_win(zen_window)
+        -- Open the file in this window
+        vim.cmd('edit ' .. vim.fn.fnameescape(file_path))
+    else
+        -- Fallback: open in the current window
+        vim.cmd('edit ' .. vim.fn.fnameescape(file_path))
+    end
+end
+
 return function() 
   local ops = {
     filters = {
@@ -58,13 +146,32 @@ return function()
       ignore_dirs = { ".git", "node_modules", ".cache", "target" },
     },
     actions = {
-      open_file = {
-        resize_window = true,
-        window_picker = {
-          enable = false,
-        }
-      },
+        open_file = {
+            window_picker = {
+                enable = false,
+            },
+            quit_on_open = false,
+        },
     },
+    on_attach = function(bufnr)
+        local api = require('nvim-tree.api')
+
+        local function opts(desc)
+            return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+        end
+
+        vim.keymap.set('n', '<CR>', function()
+            local node = api.tree.get_node_under_cursor()
+            if node.nodes ~= nil then
+                -- expand/collapse folder
+                api.node.open.edit()
+            else
+                -- open file
+                api.tree.close()
+                open_in_zen_window(node.absolute_path)
+            end
+        end, opts('Open'))
+    end,
     renderer = {
       group_empty = true,
       highlight_git = true,
