3a55d9f74a4515750bf4346f7abd8e228988d62c
Oleksii Avramenko <oleksii.avramenko.42@gmail.com>
2024-08-01 14:54:53 +0800

    update

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -19,17 +19,6 @@ local plugins = {
   },
 
   {
-    "folke/tokyonight.nvim",
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-      -- load the colorscheme here
-      vim.cmd [[colorscheme tokyonight]]
-    end,
-    enabled = false,
-  },
-
-  {
     "folke/which-key.nvim",
     lazy = false,
   },
