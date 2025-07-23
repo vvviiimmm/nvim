ab483d93822760343c63703b0584297dec20af00
Oleksii Avramenko <oleksii.avramenko@iohk.io>
2025-07-23 12:14:36 +0800

    clap boilerplate

--- lua/snippets/rust.lua
+++ lua/snippets/rust.lua
@@ -143,4 +143,42 @@ return {
       "}",
     }
   ),
+
+  
+  -- clap boiler
+  s("!clap",
+    t {
+      "use std::path::PathBuf;",
+      "",
+      "use clap::{Parser, Subcommand};",
+      "",
+      "#[derive(Parser)]",
+      "#[command(version, about, long_about = None)]",
+      "struct Cli {",
+      "    name: Option<String>,",
+      "",
+      "    #[arg(short, long, value_name = \"FILE\")]",
+      "    config: Option<PathBuf>,",
+      "",
+      "    #[arg(short, long, action = clap::ArgAction::Count)]",
+      "    debug: u8,",
+      "",
+      "#[command(subcommand)]",
+      "    command: Option<Commands>,",
+      "}",
+      "",
+      "#[derive(Subcommand)]",
+      "enum Commands {",
+      "    Test {",
+      "        #[arg(short, long)]",
+      "        list: bool,",
+      "    },",
+      "}",
+      "",
+      "fn main() {",
+      "    let cli = Cli::parse();",
+      "}",
+    }
+  )
+
 }
